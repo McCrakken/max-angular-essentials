affbbe2b09abc21af63b67a6cc9dc18f19eed0ac
McKay-Larsen <mccrakken77@gmail.com>
2024-05-29 16:17:26 -0700

    Cleanup unused imports

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,5 +1,4 @@
 import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
 
 import {TaskComponent} from "./task/task.component";
 import {TasksComponent} from "./tasks.component";
